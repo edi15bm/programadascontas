ae46ff67110932152fcebe8a8135293015c6269c
ediana <ediana.cruz@etec.sp.gov.br>
2024-04-23 17:25:16 -0300

    alterações

---
+++ prog1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+     int numero1;
+     int numero2;
+     int resultado;
+     printf("Digite um numero:\n");
+     scanf("%d",&numero1);     
+     printf("Digite outro numero:");
+     scanf("%d",&numero2);
+     resultado=numero1-numero2;
+     printf("\na subtração de %d e %d,resulta em: %d\n",numero1,numero2,resultado);
+
+
+    } 
